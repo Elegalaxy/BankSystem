3ffdd03a3da4724b35fd4993b8c00c004b76cbf8
Elegalaxy <yokai.czk@gmail.com>
2021-04-01 22:13:30 +1030

    just test file

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "TimeFunctions.h"
+using namespace std;
+
+int main(){
+	cout << getDate() << endl;
+	cout << getDay() << endl;
+	cout << getMonth() << endl;
+	cout << getYear() << endl;
+	return 0;
+}
